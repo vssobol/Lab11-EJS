3e778ed5fcbfb627147a11dc632f85fcedd09edc
carolineanneok <carolineanneok@github.com>
2019-08-13 20:59:13 -0700

    updated files

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // load the things we need
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+const PORT=process.env.PORT||3000;
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('./public'));
+//message
+
+app.post('/contact',(request, Response)=>{
+	console.log(request.body);
+	Response.sendFile('./thanks.html',{root:'./public'});
+}
+
 // use res.render to load up an ejs view file
 
 // index page 
-app.get('/', function(req, res) {
+app.get('/',(Response, res) {
 	res.render('pages/index');
 });
 
@@ -16,6 +26,6 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
 	res.render('pages/about');
 });
-
+let list =[]
 app.listen(8080);
 console.log('8080 is the magic port');
